dfea194a50b4baece094dad66f36a75dddb52a48
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-10-04 12:19:14 +0530

    Add files via upload

---
+++ oct1/percentage.py
@@ -0,0 +1,14 @@
+print("mark of each subject  in 20")
+tt=100
+a,b,c,d,e = map(int,input("Enter five values: ").split())
+print("mark of a: ", a)
+print("mark of b: ", b)
+print("mark of c: ", c)
+print("mark of d: ", d)
+print("mark of e: ", e)
+total=a+b+c+d+e
+percentage=int((total/tt)*100)
+
+print("percentage of mark is:",percentage,"%")
+
+
