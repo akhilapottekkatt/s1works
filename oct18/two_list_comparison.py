c28c8557c642b389a305763fe3cab88d90e95459
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-10-18 16:28:52 +0530

    Add files via upload

---
+++ oct18/two_list_comparison.py
@@ -0,0 +1,20 @@
+a=[]
+b=[]
+
+s1=int(input("enter the size"))
+s2=int(input("enter the size of second array"))
+for i in range(0,s1):
+    c=int(input("enter the elements"))
+    a.append(c)
+print("first array",a)
+
+for i in range(0,s2):
+    d=int(input("enter the values"))
+    b.append(d)
+print("second array",b)
+print("check the length\n" )
+print("length of first array",len(a))
+print("lenth of second array",len(b))
+    
+    
+    
