9a3d8bc0c1656dab12071d676c6099a67ea1dfff
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-10-18 16:03:11 +0530

    Add files via upload

---
+++ oct18/occcu_num.py
@@ -0,0 +1,14 @@
+count=0
+a=input("enter the text")
+b=a.split()
+print(b)
+for i in b:
+    for j in range(1,len(b)):
+        if i==b[j]:
+            
+        else:
+            
+           
+
+            
+        
