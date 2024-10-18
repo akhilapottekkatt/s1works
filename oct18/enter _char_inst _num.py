16c92c992ac197bf5b506f416ff0e91709e7b887
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-10-18 16:13:20 +0530

    Add files via upload

---
+++ oct18/enter _char_inst _num.py
@@ -0,0 +1,12 @@
+a=[]
+size=int(input("size of list"));
+for i in range(0,size):
+        b=int(input("enter the values"));
+        if b<=100:
+            a.append(b)
+
+        else:
+            a.append("$")
+
+print(a)
+        
