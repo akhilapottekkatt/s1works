1af21700522d21091a4e64c498a9605d217d54bf
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-10-27 11:28:17 +0530

    Update exchange_lastchar.py

--- oct23/exchange_lastchar.py
+++ oct23/exchange_lastchar.py
@@ -1,18 +1,22 @@
-a=input("enter the string")
-b=[]
-c=[]
-for i in a:
-    b.append(i)
-print(b)
-print(b[0])
-print(b[-1])
-c.append(b[-1])
-for i in b:
-    if i!=b[0] and i!=b[-1]:
-        c.append(i)
-c.append(b[0])
-print(c)
+# a=input("enter the string")
+# b=[]
+# c=[]
+# for i in a:
+#     b.append(i)
+# print(b)
+# print(b[0])
+# print(b[-1])
+# c.append(b[-1])
+# for i in b:
+#     if i!=b[0] and i!=b[-1]:
+#         c.append(i)
+# c.append(b[0])
+# print(c)
         
+a=input("enter the string")
+print(len(a))
+print(a[-1])
+print(a[-1]+a[1:-1]+a[0])
 
 
     
