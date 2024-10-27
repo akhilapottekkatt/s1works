ea69c12f38090d530db279a1ff967b0e7800f2ea
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-10-27 10:56:42 +0530

    Update min_max.py

--- oct23/min_max.py
+++ oct23/min_max.py
@@ -5,10 +5,10 @@ for i in range(size):
     a=int(input("enter values"))
     b.append(a)
 
-print("find the max value",max(b))
-print("find the minimum value",min(b))
-print("find the count of 1 in list",b.count(1))
-print("find the sum of list",sum(b))
+print(" max value in list",max(b))
+print("minimum value in list",min(b))
+print(" count of 1 in list",b.count(1))
+print(" sum of list",sum(b))
 
 f=int(input("enter the number to count"))
 for i in b:
