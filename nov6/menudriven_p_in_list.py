b7547aa77eaec5f1f9d1df402ffa212d58fe3625
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-11-06 13:56:58 +0530

    Add files via upload

---
+++ nov6/menudriven_p_in_list.py
@@ -0,0 +1,32 @@
+list1=[]
+newlist=[]
+n=int(input("enter the range of number"))
+for i in range(1,n+1):
+    a=int(input("enter values"))
+    list1.append(a)
+
+print(list1)
+while True:
+    print("\n select your choice")
+    print("1.find greatest and lowest number")
+    print("2.sort acending order")
+    print("3.create new list of even numbers")
+    print("4.exit")
+    choice = int(input("enter the values"))
+    if choice == 1:
+        print("the greatest value is",max(list1))
+        print("the lowest value is",min(list1))
+    elif choice ==2:
+        print("ascending order list",sorted(list1))
+    elif choice ==3:
+        for i in list1:
+            if i%2==0:
+                newlist.append(i)
+        print("even numbers new list",newlist)
+    else:
+        break
+                
+        
+        
+    
+    
