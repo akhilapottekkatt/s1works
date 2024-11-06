b6657d3598effc8ba7eac1d47f0cd1ec0cc7af16
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-11-06 16:11:42 +0530

    Add files via upload

---
+++ nov6/fibo.function.py
@@ -0,0 +1,16 @@
+def fibo(d):
+    a=0
+    b=1
+    c=0
+    e=[]
+    d=int(input("enter the number of terms in series"))
+    for i in range (d):
+        if (i==0 or i==1):
+            e.append(i)
+        else:
+            c=a+b
+            e.append(c)
+            a=b
+            b=c
+    print(e)
+fibo(5)  
