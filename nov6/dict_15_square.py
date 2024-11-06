931c8b34ad3b207d679b3e48427b16172ddb0e32
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-11-06 13:39:26 +0530

    Add files via upload

---
+++ nov6/dict_15_square.py
@@ -0,0 +1,35 @@
+dict={}
+tt=int(input("enter the total number"))
+for i in range(1,16):
+    dict[i] = i*i
+print(dict)
+
+
+dict1={}
+dict1["name"]="akhila"
+dict1["roll no"]=30
+dict1["reg"]=18901
+dict1["semester"]="s1"
+print(dict1)
+
+if tt>=90:
+    dict1["total mark"]="A"
+elif tt>=82 and tt<=89:
+    dict1["total mark"]="B"
+
+elif tt>=75 and tt<=81:
+    dict1["total mark"]="C"
+elif tt>=60 and tt<=74:
+    dict1["total mark"]="D"
+elif tt>=50 and tt<=59:
+    dict1["total mark"]="PASS"
+else:
+    dict1["total mark"]="FAIL"
+
+print(dict1)
+    
+
+
+
+
+
