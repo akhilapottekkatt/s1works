b08603d3d8b174351e080604141ae8d17fdbf627
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-11-20 15:51:56 +0530

    Update package_import.py

--- nov15/package_import.py
+++ nov15/package_import.py
@@ -1,7 +1,6 @@
 from package import facto_package as fp
 from package import fibo_function as ff
-x=fp.facto(4)
-print(x)
-y=ff.fibo(6)
-print(y)
-
+a=int(input("enter the factorial range"))
+b=int(input("enter the fibbinocci range"))
+fp.facto(a)
+ff.fibo(b)
