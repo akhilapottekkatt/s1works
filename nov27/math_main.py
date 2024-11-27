028234ad639f02de26aac66ae86a678d7b617412
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-11-27 11:57:31 +0530

    Add files via upload

---
+++ nov27/math_main.py
@@ -0,0 +1,15 @@
+from math_operations.basic_operations import arithmetic
+from math_operations.geometry import area
+print("Arithmetic operations")
+print(arithmetic.sum(3,5))
+print(arithmetic.sub(10,7))
+print(arithmetic.mul(5,3))
+print(arithmetic.div(8,2))
+print(arithmetic.sum(8,0))
+
+print("area calculation")
+
+print(area.rect(5,3))
+print(area.circle_area(7))
+print(area.tria(4,5))
+
