4e3cbe79e30dbd6267c02bd78f854ac16e717c94
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-11-27 11:54:07 +0530

    Add files via upload

---
+++ nov27/area.py
@@ -0,0 +1,9 @@
+def rect(a,b):
+    return a*b
+def circle_area(radius):
+    from math import pi
+    return pi*radius*radius
+def tria(b,h):
+    return int(0.5*b*h)
+
+
---
+++ nov27/main.py
@@ -0,0 +1,15 @@
+from basic_operations import arithmetic
+from geometry import area
+print("Arithmetic operations")
+print(arithmetic.sum(3,5))
+print(arithmetic.sub(10,7))
+print(arithmetic.mul(5,3))
+print(arithmetic.div(8,2))
+print(arithmetic.sum(8,0))
+
+print("area calculation")
+
+print(area.rect(5,3))
+print(area.circle_area(7))
+print(area.tria(4,5))
+
