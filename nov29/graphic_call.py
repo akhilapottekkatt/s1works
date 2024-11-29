6fea7ef6211e52766d2afb3e0decad177974f6ca
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2024-11-29 16:11:38 +0530

    Add files via upload

---
+++ nov29/graphic_call.py
@@ -0,0 +1,12 @@
+from graphics.circle  import circle
+from graphics.rectancle import rectangle
+from graphics.gra_3D.cuboid import cuboid
+from graphics.gra_3D.sphere import sphere
+
+
+print("details of circle",circle(4))
+print("details of rectangle",rectangle(4,5))
+print("details of cuboid",cuboid(3,5,7))
+print("details of sphere",sphere(5))
+
+
